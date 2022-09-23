78b549aa1d3dc80db722d2c7a9a2f186fb6cc8ed
ALPER TELEF <108796164+alperrtelef@users.noreply.github.com>
2022-09-23 14:05:25 +0300

    Create flatten.py

---
+++ flatten.py
@@ -0,0 +1,12 @@
+
+def flatten(n):
+    for i in n:
+        if isinstance(i,list):
+            flatten(i)
+        else:
+            empty_list.append(i)
+      
+n=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+empty_list = []
+flatten(n)
+print(empty_list)
